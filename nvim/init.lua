36c7dd0fc7c26f99284b3a0f4f32f006b7cfea18
Andy <73871814+Andy-yun-liang@users.noreply.github.com>
2026-01-03 21:07:01 -0800

    Removed auto indent

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,6 +5,10 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.autoindent = true 
+vim.opt.smartindent = true
 vim.opt.expandtab = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
@@ -127,7 +131,7 @@ vim.cmd.colorscheme("tokyonight")
 require("nvim-treesitter.configs").setup({
   ensure_installed = { "python", "javascript" },
   highlight = { enable = true },
-  indent = { enable = true },
+  indent = { enable = false },
 })
 
 -------------------------------------------------------------
